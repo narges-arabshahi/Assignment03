1c7d6df5cf29ff9cb19d23d73ba2337c5153042d
narges_arabshahi1995 <107246403+narges-arabshahi@users.noreply.github.com>
2022-06-25 23:22:59 +0430

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,7 +1,19 @@
 number=[]
-x=int(input("Please enter n: "))
-for i in range(x):
-    user=int(input("Please enter number: "))
-    number.append(user)
-    number.sort()
+m=""
+while True:
+    x=int(input("Please enter number or -1 to exit: "))
+    if x==-1:
+        break
+    else:
+        number.append(x)
+    j=number[0]
 print(number)
+for i in range(1,len(number)):
+    if j>number[i]:
+        m="array is not sort"
+        print(m)
+        break
+    else:
+        j=number[i]
+if m=="":
+    print("array is sort")
